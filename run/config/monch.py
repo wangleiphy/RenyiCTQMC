f749b1b4702368d50dd18aec11c5659e1050b257
Lei Wang <wangleiphy@gmail.com>
2014-05-07 16:13:17 +0200

    submit L=6 honeycomb with changing width W

--- run/config/monch.py
+++ run/config/monch.py
@@ -10,21 +10,22 @@ ZtoW = 0.35
 WtoZ = 0.35
 eta = 0.5
 
-latticename = 'open chain lattice'
+#latticename = 'open chain lattice'
 #latticename = 'open honeycomb lattice'
 #latticename = 'cylindrical honeycomb lattice'
+latticename = 'honeycomb lattice'
 ###############################
 nickname = 'logweight'
 
-Llist = [64]
-Wlist = [1]
+Llist = [6]
+Wlist = [15]
 
-NAlist = [32]
+NAlist = [90]
 
-Tlist = [0.5]
+Tlist = [0.1]
 #Vlist = arange(0.1, 1.6, 0.2)
-#Vlist = [1.4, 1.5]
-Vlist = arange(0.5, 10., 0.5)
+Vlist = [0.5, 1.0, 2.0]
+#Vlist = arange(0.5, 10., 0.5)
 
 NSKIP = 200
 THERMALIZATION = 10**5
