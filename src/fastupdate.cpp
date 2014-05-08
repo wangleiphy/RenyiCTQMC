4df3f1054328f2bf29c573d3ab3e484b00dc0e06
Lei Wang <wangleiphy@gmail.com>
2014-05-08 09:00:55 +0200

    cache Stilde R, MQ; now before add/remove we have to compute the weight

--- src/fastupdate.cpp
+++ src/fastupdate.cpp
@@ -61,58 +61,61 @@ double InteractionExpansion::Zadd_impl(const double tau, const std::vector<site_
 {
  
     //ratio of Z, it depends on whether tau is < beta or not 
-    unsigned int icopy = tau < beta ? 0: 1;  
-    unsigned int Msize = M[icopy].matrix().rows();
- 
-    Eigen::Matrix2d Stilde = Eigen::Matrix2d::Zero(); // diagonal term is always zero 
-    Eigen::Matrix2Xd RM(2, Msize), R(2, Msize);
-    Eigen::MatrixX2d Q(Msize, 2), MQ(Msize, 2);
- 
-    Stilde(0,1) = green0_spline(0., sites[0], sites[1]);
-    Stilde(1,0) = -Stilde(0,1)* lattice.parity(sites[0])* lattice.parity(sites[1]);   
- 
-    //std::cout << "Stilde:\n"<< Stilde << std::endl; 
-       
-    for(unsigned int i=0; i<2; ++i){
-     for(unsigned int j=0; j< Msize; ++j){
-          Q(j,i) = green0_spline(M[icopy].creators()[j].t()-tau, M[icopy].creators()[j].s(), sites[i]);
-          R(i,j) = -lattice.parity(sites[i]) * M[icopy].creators()[j].parity()* Q(j,i);//anti-symmetrization 
-      }
-    }
- 
-    if(Msize>0){
-      MQ.noalias() = M[icopy].matrix() * Q; 
-      Stilde.noalias() -= R * MQ; 
-    }
+    unsigned icopy = tau < beta ? 0: 1;  
+    unsigned Msize = M[icopy].matrix().rows();
 
-  //return weight if we have nothing else to do
-  if(compute_only_weight){
-    return Stilde.determinant();// we have not yet perform the inverse, so it is actually 1./det(Stilde)
+    if(compute_only_weight){
+
+         Eigen::MatrixX2d Q(Msize, 2);
+
+         M[icopy].Stilde = Eigen::Matrix2d::Zero(); // diagonal term is always zero 
+         M[icopy].R.resize(2, Msize);
+         M[icopy].MQ.resize(Msize, 2);
+
+         
+         M[icopy].Stilde(0,1) = green0_spline(0., sites[0], sites[1]);
+         M[icopy].Stilde(1,0) = -M[icopy].Stilde(0,1)* lattice.parity(sites[0])* lattice.parity(sites[1]);   
+         
+         //std::cout << "Stilde:\n"<< Stilde << std::endl; 
+            
+         for(unsigned i=0; i<2; ++i){
+          for(unsigned j=0; j< Msize; ++j){
+               Q(j,i) = green0_spline(M[icopy].creators()[j].t()-tau, M[icopy].creators()[j].s(), sites[i]);
+               M[icopy].R(i,j) = -lattice.parity(sites[i]) * M[icopy].creators()[j].parity()* Q(j,i);//anti-symmetrization 
+           }
+         }
+         
+         if(Msize>0){
+           M[icopy].MQ.noalias() = M[icopy].matrix() * Q; 
+           M[icopy].Stilde.noalias() -= M[icopy].R * M[icopy].MQ; 
+         }
+         
+         return M[icopy].Stilde.determinant();// we have not yet perform the inverse, so it is actually 1./det(Stilde)
   }
-
  
-  Stilde = Stilde.inverse().eval(); 
+  M[icopy].Stilde = M[icopy].Stilde.inverse().eval(); 
 
+  Eigen::Matrix2Xd RM(2, Msize);
   if (Msize>0)
-      RM.noalias() = R*M[icopy].matrix(); 
+      RM.noalias() = M[icopy].R*M[icopy].matrix(); 
 
   M[icopy].matrix().conservativeResize(Msize+2, Msize+2);//conservativeResize keep the content 
 
   //perform the actual update  
   if(Msize>0){
-    M[icopy].matrix().topRightCorner(Msize, 2).noalias()   = -MQ * Stilde ;       
+    M[icopy].matrix().topRightCorner(Msize, 2).noalias()   = -M[icopy].MQ * M[icopy].Stilde ;       
     M[icopy].matrix().topLeftCorner(Msize, Msize).noalias() -=  M[icopy].matrix().topRightCorner(Msize, 2) * RM; 
-    M[icopy].matrix().bottomLeftCorner(2, Msize).noalias() = -Stilde * RM; 
+    M[icopy].matrix().bottomLeftCorner(2, Msize).noalias() = -M[icopy].Stilde * RM; 
   }
 
-  M[icopy].matrix().bottomRightCorner(2, 2) = Stilde; 
+  M[icopy].matrix().bottomRightCorner(2, 2) = M[icopy].Stilde; 
 
-  for(unsigned int i=0; i<2; ++i)
+  for(unsigned i=0; i<2; ++i)
      M[icopy].creators().push_back(creator(sites[i], lattice.parity(sites[i]), tau)); 
 
   M[icopy].num_vertices() += 1;
 
-  return 1./Stilde.determinant();
+  return 1./M[icopy].Stilde.determinant();
 }
 
 /*implement add vertex in W sector*/
@@ -122,58 +125,60 @@ double InteractionExpansion::Wadd_impl(const double tau, const std::vector<site_
     assert(Msuper.matrix().rows() == Msuper.matrix().cols()); 
     assert(Msuper.matrix().rows() == Msuper.creators().size()); 
 
-    unsigned int Msize = Msuper.matrix().rows();
+    unsigned Msize = Msuper.matrix().rows();
  
-    Eigen::Matrix2d Stilde = Eigen::Matrix2d::Zero(); // diagonal term is always zero 
-    Eigen::Matrix2Xd RM(2, Msize), R(2, Msize);
-    Eigen::MatrixX2d Q(Msize, 2), MQ(Msize, 2);
- 
-    Stilde(0, 1) = super_green0_spline(tau, tau, sites[0], sites[1]);  
-    //Stilde(0, 1) = super_bare_green_itime.gf(tau, tau, sites[0], sites[1]);  
-    Stilde(1, 0) = -Stilde(0, 1)* lattice.parity(sites[0])* lattice.parity(sites[1]);   
+   if(compute_only_weight){
+
+        Eigen::MatrixX2d Q(Msize, 2);
+
+        Msuper.Stilde = Eigen::Matrix2d::Zero(); // diagonal term is always zero 
+        Msuper.R.resize(2, Msize);
+        Msuper.MQ.resize(Msize, 2);
        
-    for(unsigned int i=0; i<2; ++i){
-     for(unsigned int j=0; j< Msize; ++j){
-          Q(j,i) = super_green0_spline(Msuper.creators()[j].t(), tau, Msuper.creators()[j].s(), sites[i]);
-          //Q(j,i) = super_bare_green_itime.gf(Msuper.creators()[j].t(), tau, Msuper.creators()[j].s(), sites[i]);
-          R(i,j) = -lattice.parity(sites[i]) * Msuper.creators()[j].parity()* Q(j,i);//anti-symmetrization 
-      }
-    }
- 
-    if(Msize>0){
-      MQ.noalias() = Msuper.matrix() * Q; 
-      Stilde.noalias() -= R * MQ; 
-    }
-
-  //return weight if we have nothing else to do
-  if(compute_only_weight){
-    return  Stilde.determinant();// we have not yet perform the inverse, so it is actually 1./det(Stilde)
+        Msuper.Stilde(0, 1) = super_green0_spline(tau, tau, sites[0], sites[1]);  
+        //Stilde(0, 1) = super_bare_green_itime.gf(tau, tau, sites[0], sites[1]);  
+        Msuper.Stilde(1, 0) = -Msuper.Stilde(0, 1)* lattice.parity(sites[0])* lattice.parity(sites[1]);   
+           
+        for(unsigned i=0; i<2; ++i){
+         for(unsigned j=0; j< Msize; ++j){
+              Q(j,i) = super_green0_spline(Msuper.creators()[j].t(), tau, Msuper.creators()[j].s(), sites[i]);
+              //Q(j,i) = super_bare_green_itime.gf(Msuper.creators()[j].t(), tau, Msuper.creators()[j].s(), sites[i]);
+              Msuper.R(i,j) = -lattice.parity(sites[i]) * Msuper.creators()[j].parity()* Q(j,i);//anti-symmetrization 
+          }
+        }
+        
+        if(Msize>0){
+          Msuper.MQ.noalias() = Msuper.matrix() * Q; 
+          Msuper.Stilde.noalias() -= Msuper.R * Msuper.MQ; 
+        }
+        
+        return  Msuper.Stilde.determinant();// we have not yet perform the inverse, so it is actually 1./det(Stilde)
   }
-
  
-  Stilde = Stilde.inverse().eval(); 
+  Msuper.Stilde = Msuper.Stilde.inverse().eval(); 
 
+  Eigen::Matrix2Xd RM(2, Msize);
   if (Msize>0)
-      RM.noalias() = R*Msuper.matrix(); 
+      RM.noalias() = Msuper.R*Msuper.matrix(); 
 
   Msuper.matrix().conservativeResize(Msize+2, Msize+2);//conservativeResize keep the content 
 
   //perform the actual update  
   if(Msize>0){
-    Msuper.matrix().topRightCorner(Msize, 2).noalias()   = -MQ * Stilde ;       
+    Msuper.matrix().topRightCorner(Msize, 2).noalias()   = -Msuper.MQ * Msuper.Stilde ;       
     Msuper.matrix().topLeftCorner(Msize, Msize).noalias() -=  Msuper.matrix().topRightCorner(Msize, 2) * RM; 
-    Msuper.matrix().bottomLeftCorner(2, Msize).noalias() = -Stilde * RM; 
+    Msuper.matrix().bottomLeftCorner(2, Msize).noalias() = -Msuper.Stilde * RM; 
   }
 
-  Msuper.matrix().bottomRightCorner(2, 2) = Stilde; 
+  Msuper.matrix().bottomRightCorner(2, 2) = Msuper.Stilde; 
 
-  for(unsigned int i=0; i<2; ++i){
+  for(unsigned i=0; i<2; ++i){
      Msuper.creators().push_back(creator(sites[i], lattice.parity(sites[i]), tau)); 
   }
 
   Msuper.num_vertices() += 1; 
 
-  return 1./Stilde.determinant();
+  return 1./Msuper.Stilde.determinant();
 }
 
 
@@ -187,11 +192,10 @@ double InteractionExpansion::Zremove_impl(const unsigned vertex, const bool comp
 
   unsigned Msize = M[icopy].matrix().rows();
 
-  // the block we want to remove
-  Eigen::Matrix2d Stilde = M[icopy].matrix().block<2,2>(2*vert, 2*vert);
-
   if(compute_only_weight){
-    return Stilde.determinant();
+    // the block we want to remove
+    M[icopy].Stilde = M[icopy].matrix().block<2,2>(2*vert, 2*vert);
+    return M[icopy].Stilde.determinant();
   }
 
   //swap with the back 
@@ -225,7 +229,7 @@ double InteractionExpansion::Zremove_impl(const unsigned vertex, const bool comp
   M[icopy].matrix().conservativeResize(Msize, Msize); 
 
   if(Msize>0)
-    M[icopy].matrix().noalias() -= Qtilde * Stilde.inverse() * Rtilde; 
+    M[icopy].matrix().noalias() -= Qtilde * M[icopy].Stilde.inverse() * Rtilde; 
 
   //std::cout << "remove:6" << std::endl; 
   //get rid of operators
@@ -234,7 +238,7 @@ double InteractionExpansion::Zremove_impl(const unsigned vertex, const bool comp
 
   M[icopy].num_vertices() -= 1; 
 
-  return Stilde.determinant(); 
+  return M[icopy].Stilde.determinant(); 
 }
 
 
@@ -244,11 +248,10 @@ double InteractionExpansion::Wremove_impl(const unsigned vertex, const bool comp
 
   unsigned Msize = Msuper.matrix().rows();
 
-  // the block we want to remove
-  Eigen::Matrix2d Stilde = Msuper.matrix().block<2,2>(2*vertex, 2*vertex);
-
   if(compute_only_weight){
-    return Stilde.determinant();
+    // the block we want to remove
+    Msuper.Stilde = Msuper.matrix().block<2,2>(2*vertex, 2*vertex);
+    return Msuper.Stilde.determinant();
   }
 
   //swap with the back 
@@ -282,7 +285,7 @@ double InteractionExpansion::Wremove_impl(const unsigned vertex, const bool comp
   Msuper.matrix().conservativeResize(Msize, Msize); 
 
   if(Msize>0)
-    Msuper.matrix().noalias() -= Qtilde * Stilde.inverse() * Rtilde; 
+    Msuper.matrix().noalias() -= Qtilde * Msuper.Stilde.inverse() * Rtilde; 
 
   //std::cout << "remove:6" << std::endl; 
   //get rid of operators
@@ -291,5 +294,5 @@ double InteractionExpansion::Wremove_impl(const unsigned vertex, const bool comp
 
   Msuper.num_vertices() -= 1; 
 
-  return Stilde.determinant(); 
+  return Msuper.Stilde.determinant(); 
 }
