46166a0e538887d08efaa6ebd32e992225e92d2a
Lei Wang <wangleiphy@gmail.com>
2014-05-19 10:40:41 +0200

    null change

--- run/config/brutus.py
+++ run/config/brutus.py
@@ -15,14 +15,18 @@ latticename = 'square lattice'
 ###############################
 nickname = 'ratiotrick'
 
-Llist = [4]
-Wlist = [4]
-NAstep = 4 
+Llist = [8]
+Wlist = [8]
+NAstep = 8
 
-Tlist = arange(0.3, 1.1, 0.1)
+#NA0list= [0,6,12,18,24,30]
+#NA1list= [6,12,18,24,30,36]
+
+Tlist =[0.85, 0.95, 1.05, 1.15]
+#Tlist = arange(0.6, 2.1, 0.1)
 #Vlist = arange(0.1, 1.6, 0.1)
 #Vlist = arange(2., 11., 1.)
-Vlist = [1.0]
+Vlist = [2.]
 
 Ntau = 1000
 NSKIP = 100 
@@ -34,7 +38,7 @@ Nscratch = 100
 tmin = 60
 tmax = 300
 ncores = 16 
-wtime = '12:00'
+wtime = '18:00'
 
 resfolder = '/cluster/work/scr6/lewang/renyidata/' + nickname  + '/'
 #h, m = [int(i) for i in wtime.split(':')]
