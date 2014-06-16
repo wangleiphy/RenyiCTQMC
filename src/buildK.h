7b38eb4eb21e52b72dc8ca0f1ab586faa25db99a
Lei Wang <wangleiphy@gmail.com>
2014-06-16 17:32:08 +0200

    forgot to setin pi-flux hamiltonain, resubmit

--- src/buildK.h
+++ src/buildK.h
@@ -10,10 +10,9 @@ Eigen::MatrixXd buildK(const alps::graph_helper<>& lattice){
 
     //construct the hamiltonian K 
     Eigen::MatrixXd K = Eigen::MatrixXd::Zero(lattice.num_sites(), lattice.num_sites()); 
-    BOOST_FOREACH(const alps::graph_helper<>::site_descriptor& s1, lattice.sites()) {
-        BOOST_FOREACH(alps::graph_helper<>::site_descriptor const& s2, lattice.neighbors(s1)) {
-            K(s1, s2) = -1.0; 
-        }
+    BOOST_FOREACH(const alps::graph_helper<>::bond_descriptor& b, lattice.bonds()){
+         K(lattice.source(b), lattice.target(b)) = (lattice.bond_type(b)==0) ? -1. : (lattice.bond_type(b)==1) ? 1. : 0. ;
+         K(lattice.target(b), lattice.source(b)) = K(lattice.source(b), lattice.target(b));
     }
 
    return K; 
@@ -26,10 +25,9 @@ Eigen::MatrixXd buildKAB(const alps::graph_helper<>& lattice, const unsigned NA)
 
     //construct the hamiltonian 
     Eigen::MatrixXd KAB = Eigen::MatrixXd::Zero(lattice.num_sites()+NB, lattice.num_sites()+NB); 
-    BOOST_FOREACH(const alps::graph_helper<>::site_descriptor& s1, lattice.sites()) {
-        BOOST_FOREACH(alps::graph_helper<>::site_descriptor const& s2, lattice.neighbors(s1)) {
-            KAB(s1, s2) = -1.0; 
-        }
+    BOOST_FOREACH(const alps::graph_helper<>::bond_descriptor& b, lattice.bonds()){
+         KAB(lattice.source(b), lattice.target(b)) = (lattice.bond_type(b)==0) ? -1. : (lattice.bond_type(b)==1) ? 1. : 0. ;
+         KAB(lattice.target(b), lattice.source(b)) = KAB(lattice.source(b), lattice.target(b));
     }
 
    return KAB; 
@@ -42,10 +40,9 @@ Eigen::MatrixXd buildKABprime(const alps::graph_helper<>& lattice, const unsigne
 
     //construct the hamiltonian 
     Eigen::MatrixXd KABprime = Eigen::MatrixXd::Zero(lattice.num_sites()+NB, lattice.num_sites()+NB); 
-    BOOST_FOREACH(const alps::graph_helper<>::site_descriptor& s1, lattice.sites()) {
-        BOOST_FOREACH(alps::graph_helper<>::site_descriptor const& s2, lattice.neighbors(s1)) {
-            KABprime(s1, s2) = -1.0; 
-        }
+    BOOST_FOREACH(const alps::graph_helper<>::bond_descriptor& b, lattice.bonds()){
+         KABprime(lattice.source(b), lattice.target(b)) = (lattice.bond_type(b)==0) ? -1. : (lattice.bond_type(b)==1) ? 1. : 0. ;
+         KABprime(lattice.target(b), lattice.source(b)) = KABprime(lattice.source(b), lattice.target(b));
     }
 
     //swap B and Bprime  
